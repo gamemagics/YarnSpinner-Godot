cf5a08cc17a7265d94fe7893cbfd68ee759a2133
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-01-21 16:10:48 +0800

    #feature #views #add options

--- addons/YarnSpinner/Views/RPGView.cs
+++ addons/YarnSpinner/Views/RPGView.cs
@@ -5,11 +5,21 @@ public class RPGView : DialogueViewBase {
     [Export] private string textPath;
     private RichTextLabel textLabel;
 
+    [Export] private string speakerPath;
+    private RichTextLabel speakerLabel;
+
+    [Export] private string[] optionsPath;
+    private Button[] optionsButton;
+
+    Action<int> pressedAction;
+
     public override void _Ready() {
         base._Ready();
         textLabel = GetNode<RichTextLabel>(textPath);
-        if (textLabel == null) {
-            GD.PrintErr("Can't find " + textPath);
+        speakerLabel = GetNode<RichTextLabel>(speakerPath);
+        optionsButton = new Button[optionsPath.Length];
+        for (int i = 0; i < optionsPath.Length; ++i) {
+            optionsButton[i] = GetNode<Button>(optionsPath[i]);
         }
     }
 
@@ -18,5 +28,33 @@ public class RPGView : DialogueViewBase {
         if (textLabel != null) {
             textLabel.Text = dialogueLine.TextWithoutCharacterName.Text;
         }
+        if (speakerLabel != null) {
+            speakerLabel.Text = dialogueLine.CharacterName;
+        }
+    }
+
+    public override void RunOptions(DialogueOption[] dialogueOptions, Action<int> onOptionSelected) {
+        pressedAction = onOptionSelected;
+        for (int i = 0; i < dialogueOptions.Length; ++i) {
+            optionsButton[i].Text = dialogueOptions[i].Line.Text.Text;
+            optionsButton[i].Visible = true;
+            optionsButton[i].Connect("pressed", this, "SelectOption" + i.ToString());
+        }
+    }
+
+    private void SelectOption0() {
+        for (int i = 0; i < optionsButton.Length; ++i) {
+            optionsButton[i].Visible = false;
+        }
+
+        pressedAction.Invoke(0);
+    }
+
+    private void SelectOption1() {
+        for (int i = 0; i < optionsButton.Length; ++i) {
+            optionsButton[i].Visible = false;
+        }
+
+        pressedAction.Invoke(1);
     }
 }
